5376d4427a1dd483b580d5f5087d83c9d04b08c5
Alex McKean <amckean@me.com>
2018-03-23 16:47:07 +0000

    automatically backed up by learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,11 @@
+class CreateShows < ActiveRecord::Migration 
+  def change
+    create_table :shows do |s|
+      s.string :name 
+      s.network :network
+      s.datetime :day
+      s.integer :rating
+  end 
+  
+end 
+
